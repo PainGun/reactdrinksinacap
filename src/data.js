5edef534b5dcfe5e4c71c8fb9e94aa47abc95458
Felipe <felipe.duarte.acevedo@gmail.com>
2022-07-18 14:25:08 -0400

    Primer commit, creacion proyecto Inacap

---
+++ src/data.js
@@ -0,0 +1,116 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Score",
+    img: "img/product-1.png",
+    price: 1100,
+    company: "Score Energy Drink Ltda",
+    info:
+      "Pensando en nuestro público fanático de las bebidas refrescantes, Score te trae una energética única en Sudamérica. Ideal para tomarla durante la fiesta, sola o mezclada, Score te da ese plus energético necesario después de un día largo. La energía que necesitas con el sabor tradicional. Formato 500 cc",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Mr. Big",
+    img: "img/product-2.png",
+    price: 1100,
+    company: "Mr. Big Energy Drinks Ltda.",
+    info:
+      "MR. BIG, a diferencia de las bebidas isotónicas, es una bebida energizante que estimula el cuerpo para predisponerlo a realizar actividades físicas, pero no tiene la facultad de rehidratar el cuerpo, por lo que se recomienda para tal efecto el consumo abundante de agua.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Monster Energy",
+    img: "img/product-3.png",
+    price: 1490,
+    company: "Monster Energy Company",
+    info:
+      "Prueba una de las bebidas más extremas del planeta, Monster Energy! Es una combinación ideal de ingredientes de proporciones exactas que ofrecen el Monster boost de energía que tanto le gusta a nuestros fanáticos. Monster tiene un sabor peculiar. ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Frescolita",
+    img: "img/product-4.png",
+    price: 1100,
+    company: "Frescolita Ltda.",
+    info:
+      "Refresco Frescolita, es un tipo de bebida gaseosa dulce de color rojo con un tinte naranja. Su sabor es una mezcla entre tutti-frutti, vainilla y chicle.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Red Bull",
+    img: "img/product-5.png",
+    price: 1350,
+    company: "Red Bull Company",
+    info:
+      "Red Bull® Energy Drink - especialmente elaborado para los momentos de mayor esfuerzo. Red Bull es reconocido por toda dase de atletas, por profesionales y estudiantes activos, como por conductores que cubren largas distancias alrededor del mundo.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Rock Star Energy Drink",
+    img: "img/product-6.png",
+    price: 1500,
+    company: "Rock Star",
+    info:
+      "Rockstar Energy Drink es una de las marcas de bebidas energéticas más vendidas a nivel mundial. Es una marca de Lifestyle con el apoyo de las mejores campañas de marketing para el consumidor.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Shot",
+    img: "img/product-7.png",
+    price: 1300,
+    company: "Shot & Go",
+    info:
+      "Shot&Go se produce exclusivamente en Austria y se exporta a todo el mundo. Todos los ingredientes son aprobados y producidos por compañías farmacéuticas. La alta calidad, la seguridad y la confianza son factores claves en el proceso de producción.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Burn",
+    img: "img/product-8.png",
+    price: 1400,
+    company: "Cocacola Company",
+    info:
+      "Burn es una bebida energética de Monster Energy distribuida por The Coca-Cola Company. Está enfocada a un público joven, principalmente Millenials y Gen-Z. Sus valores de marca se sustentan en tres pilares: música, baile y lifestyle.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Score",
+  img: "img/product-1.png",
+  price: 1100,
+  company: "Score Energy Drink Ltda",
+  info:
+    "Pensando en nuestro público fanático de las bebidas refrescantes, Score te trae una energética única en Sudamérica. Ideal para tomarla durante la fiesta, sola o mezclada, Score te da ese plus energético necesario después de un día largo. La energía que necesitas con el sabor tradicional. Formato 500 cc",
+  inCart: false,
+  count: 0,
+  total: 0
+};
